5105d2f1b6ac331a92ae6be8a4781acd9f5868d1
Angeluz20 <estevao.silva015@gmail.com>
2024-08-02 21:19:21 -0400

    [fix] Events page under development

---
+++ src/app/PageEvents/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/header/page";
+
+export default function Events(){
+    return (
+        <div>
+              <Header/>
+            <div>
+                <h1>Eventos</h1>
+                {/* Conteúdo dos eventos */}              
+            </div>
+        </div>
+    )
+}
