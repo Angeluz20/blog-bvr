fcc95795be9f631e34ad68a139b644355907a6a4
Angeluz20 <estevao.silva015@gmail.com>
2024-08-02 21:27:15 -0400

    [feat] Add component Header

---
+++ src/components/header/page.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Image from "next/image";
+import Head from "next/head";
+import LogoBVR from "../../../public/assets/logo-bvr.png";
+import TitleLogo from "../../../public/assets/titlebvr.png";
+
+const NavLink = ({ name, route } : any) => (
+  <a
+    key={name}
+    href={route}
+    className="text-white px-5 py-3 hover:h-12 hover:bg-[#195262] hover:rounded-md"
+    aria-label={`Navigate to ${name}`}
+  >
+    {name}
+  </a>
+);
+
+const Navigation = ({ pages }: any) => (
+  <nav className="flex justify-around">
+    {pages.map((item : any) => (
+      <NavLink key={item.name} name={item.name} route={item.route} />
+    ))}
+  </nav>
+);
+
+export default function Header() {
+  const headerPagesName = [
+    { name: "Início", route: "/" },
+    { name: "Notícias", route: "/News" },
+    { name: "Eventos", route: "/PageEvents" },
+    { name: "Obras", route: "/obras" },
+    { name: "Turismo", route: "/turismo" },
+  ];
+
+  return (
+    <header className="flex shadow-2xl items-center justify-between p-8 h-20 w-full bg-gradient-to-r from-[#092932] to-[#0B495A]">
+      <div className="flex">
+         <Image src={LogoBVR} alt={"Logo"} className="w-16"/> 
+         <Image src={TitleLogo} alt={"Title"} className="w-30"/> 
+      </div>
+      <Navigation pages={headerPagesName} />
+    </header>
+  );
+}
