5fe3084b6bfbfbf6f2358e956c074457d0ce8ae5
Angeluz20 <estevao.silva015@gmail.com>
2024-08-04 23:12:59 -0400

    [feat] add description to TitlePage component

--- src/components/titlePage/page.tsx
+++ src/components/titlePage/page.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 
 type TitleProps = {
-    title: string;
-    subtitle: string;
+  title: string;
+  subtitle: string;
+  description?: string;
 }
 
-export default function TitlePage({ title, subtitle}: TitleProps){
-    return(
-        <>
-            <h1 className="text-2xl text-[#092932]">
-                <strong>{title}</strong>
-            </h1>
-            <h3 className="text-gray-400">{subtitle}</h3>
-        </>
-    )
+export default function TitlePage({ title, subtitle, description }: TitleProps) {
+  return (
+    <div className="flex flex-col items-center p-6 w-full">
+      <h1 className="text-2xl text-[#092932]">
+        <strong>{title}</strong>
+      </h1>
+      <h3 className="text-gray-400">{subtitle}</h3>
+      <p className="w-3/5 text-center">{description}</p>
+    </div>
+  )
 }
