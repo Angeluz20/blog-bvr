d5363ded4102510d9de11e14757bd9da5732ce92
Angeluz20 <estevao.silva015@gmail.com>
2024-08-11 19:46:29 -0400

    [feat] integrate mock data, update responsive layout, and add search field with functional search on News page (filter buttons pending)

--- src/app/News/page.tsx
+++ src/app/News/page.tsx
@@ -1,44 +1,84 @@
-import React from "react";
+"use client";
+import React, { useMemo, useState } from "react";
 import TitlePage from "@/components/titlePage/page";
-import ExampleNoticie from "../../../public/assets/example-noticie.jpeg";
-import { NewsProps } from "@/utils/types/news";
+import { NewsProps } from "../../@types/news";
+import { CiSearch } from "react-icons/ci";
+import { MdOutlineHealthAndSafety } from "react-icons/md";
+import { FaRunning, FaPalette, FaTheaterMasks } from "react-icons/fa"; 
 import CardNews from "@/components/cardNews/page";
-
-export const newsData: NewsProps[] = [
-    { id: "1", title: "Nova Parceria Anunciada", date: "05/08/2024", poster: ExampleNoticie, description: "A empresa anunciou uma nova parceria estratégica com a Tech Innovators." },
-    { id: "2", title: "Lançamento de Produto", date: "12/08/2024", poster: ExampleNoticie, description: "O novo produto promete revolucionar o mercado com suas funcionalidades avançadas." },
-    { id: "3", title: "Conferência de Tecnologia", date: "18/08/2024", poster: ExampleNoticie, description: "A conferência anual de tecnologia atraiu milhares de participantes de todo o mundo." },
-    { id: "4", title: "Prêmios de Inovação", date: "22/08/2024", poster: ExampleNoticie, description: "Nossa empresa recebeu vários prêmios por suas inovações no setor de tecnologia." },
-    { id: "5", title: "Expansão Internacional", date: "25/08/2024", poster: ExampleNoticie, description: "Estamos expandindo nossas operações para novos mercados na Ásia e Europa." },
-    { id: "6", title: "Nova Filial Aberta", date: "28/08/2024", poster: ExampleNoticie, description: "Uma nova filial foi aberta em São Paulo, aumentando nossa presença no Brasil." },
-    { id: "7", title: "Campanha de Marketing", date: "30/08/2024", poster: ExampleNoticie, description: "Lançamos uma campanha de marketing inovadora que está atraindo muita atenção." },
-    { id: "8", title: "Relatório Anual de Sustentabilidade", date: "02/09/2024", poster: ExampleNoticie, description: "O relatório deste ano destaca nossos esforços contínuos para a sustentabilidade." },
-];
-
+import { newsData } from "@/service/api";
 
 export default function News() {
+    const [searchItem, setSearchItem] = useState("");
+
+    const filteredNews = useMemo(() => {
+        const data = newsData.filter((news) =>
+            news.title.toLowerCase().includes(searchItem.toLowerCase())
+        );
+        return data.sort((a, b) => a.title.localeCompare(b.title));
+    }, [searchItem]);
+
+    // Array de categorias com ícones
+    const category = [
+        { id: "1", title: "Saúde", icon: <MdOutlineHealthAndSafety size={30} color="#083344" /> },
+        { id: "2", title: "Esporte", icon: <FaRunning size={30} color="#083344" /> },
+        { id: "3", title: "Cultura", icon: <FaPalette size={30} color="#083344" /> },
+        { id: "4", title: "Diversas", icon: <FaTheaterMasks size={30} color="#083344" /> },
+    ];
+
     return (
-        <div>
-            <main className="flex flex-col p-8">
-                <div className="mb-4 flex items-center flex-col">
-                    <TitlePage title={"Notícias"} subtitle={"Explore as notícias da cidade"} />
-                </div>
-                <div className="flex items-center justify-center p-10">
-                    <div className="grid grid-cols-4 gap-20 justify-between w-full flex -mx-4">
-                        {newsData.map(({ id, title, date, poster }: NewsProps) => (
-                            <CardNews
-                                key={id}
-                                title={title}
-                                date={date}
-                                poster={poster}
-                                description={"Fill your designs with placeholders in a click: text, images and even data! Generate 'Lorem ipsum' to fill your text layers,Fill any shape with the perfect ..."}
-                                isDescription={true}
-                                idDetails={id}
+        <div className="flex flex-col min-h-screen mb-4 mt-20">
+            <div className="flex flex-col flex-grow w-full items-center">
+                <header className="mb-4 flex flex-col w-full bg-[#e8ecec] items-center">
+                    <TitlePage title="Notícias" subtitle="Explore as notícias da cidade" />
+                    <div className="flex xl:w-2/3 items-center justify-center xl:flex-row flex-col mb-10 w-full">
+                        <div className="flex shadow p-1 items-center justify-center rounded-xl w-2/3 max-w-lg bg-background mb-4">
+                            <CiSearch size={35} className="text-gray-500 m-2" />
+                            <input
+                                type="text"
+                                value={searchItem}
+                                onChange={(e) => setSearchItem(e.target.value)}
+                                className="outline-none rounded-xl bg-background w-full h-12"
+                                placeholder="Pesquisar..."
                             />
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap justify-center items-center w-full max-w-2xl gap-4 mb-8">
+                        {category.map((item) => (
+                            <div
+                                key={item.id}
+                                className="flex cursor-pointer flex-col w-20 h-20 sm:w-32 sm:h-32 hover:border-1 hover:border-[#0d9488] shadow-xl bg-background rounded-2xl justify-center items-center transition-transform transform hover:scale-105"
+                            >
+                                {item.icon}
+                                <p className="text-[#083344] text-center text-sm sm:text-base">{item.title}</p>
+                            </div>
                         ))}
                     </div>
-                </div>
-            </main>
+                </header>
+
+                <main className="flex flex-grow w-full flex-col justify-center items-center">
+                    <section className="flex flex-wrap gap-16 justify-center items-center mt-8 w-full max-w-7xl">
+                        {filteredNews.length > 0 ? (
+                            filteredNews.map(({ id, title, date, poster }: NewsProps) => (
+                                <CardNews
+                                    key={id}
+                                    title={title}
+                                    date={date}
+                                    poster={poster}
+                                    description="Fill your designs with placeholders in a click: text, images and even data! Generate 'Lorem ipsum' to fill your text layers,Fill any shape with the perfect ..."
+                                    isDescription={true}
+                                    id={id}
+                                />
+                            ))
+                        ) : (
+                            <div className="flex justify-center items-center h-full">
+                                <p className="text-lg text-gray-500">Nenhum resultado encontrado</p>
+                            </div>
+                        )}
+                    </section>
+
+                </main>
+            </div>
         </div>
     );
 }
