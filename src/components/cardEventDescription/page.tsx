e781840cc9a0151bebf67891be9fc93c13635428
Angeluz20 <estevao.silva015@gmail.com>
2024-08-11 19:50:15 -0400

    [refactor] refactor CardEventDescription component

--- src/components/cardEventDescription/page.tsx
+++ src/components/cardEventDescription/page.tsx
@@ -1,25 +1,18 @@
 import React from "react";
-import Image from "next/image";
-import { IEventProps } from "@/utils/types/events";
+import { IEventProps } from "../../@types/events";
 
-export default function CardDescription({ title, poster, description }: IEventProps) {
+export default function CardDescriptionPage({ title, description }: IEventProps) {
   return (
-    <div className="w-3/4 flex h-96 mb-32">
-      <div className="w-full shadow flex justify-between rounded-md bg-white h-full p-5 mb-10">
-        <div className="relative w-2/3 h-full rounded-md overflow-hidden">
-          <Image
-            src={poster}
-            alt="poster"
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
-        <div className="flex flex-col w-2/3 text-gray-500 text-sm p-5">
-          <div className="flex mb-5">
-            <p className="text-3xl text-black"><strong>{title}</strong></p>
+    <div className="w-full items-center justify-center p-5 flex h-auto md:h-96">
+      <div className="w-full flex flex-col md:flex-row md:items-center justify-between rounded-xl h-full p-5">
+        <div className="flex flex-col md:ml-5 w-full items-center text-white text-sm p-5 rounded-xl">
+          <div className="flex mb-5 w-full md:w-3/4">
+            <p className="text-2xl md:text-4xl text-white text-center font-bold">
+              {title}
+            </p>
           </div>
-          <div className="w-full flex flex-col">
-            <p className="text-justify text-xl">
+          <div className="flex flex-col w-full md:w-3/4">
+            <p className="text-justify text-base md:text-xl">
               {description}
             </p>
           </div>
