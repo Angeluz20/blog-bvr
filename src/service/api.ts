421a6acaa1844c1de7842debf24265c7a5149954
Angeluz20 <estevao.silva015@gmail.com>
2024-08-11 19:41:20 -0400

    [feat] create api.ts to simulate mock data

---
+++ src/service/api.ts
@@ -0,0 +1,67 @@
+import ExmapleNews from "../../public/assets/example-noticie.jpeg"
+export const newsData = [
+    {
+        id: "1",
+        title: "Nova Parceria Anunciada",
+        date: "05/08/2024",
+        category: "Saúde",
+        poster: ExmapleNews,
+        description: "A empresa anunciou uma nova parceria estratégica com a Tech Innovators."
+    },
+    {
+        id: "2",
+        title: "Lançamento de Produto",
+        date: "12/08/2024",
+        category: "Saúde",
+        poster: ExmapleNews,
+        description: "O novo produto promete revolucionar o mercado com suas funcionalidades avançadas."
+    },
+    {
+        id: "3",
+        title: "Conferência de Tecnologia",
+        date: "18/08/2024",
+        category: "Saúde",
+        poster: ExmapleNews,
+        description: "A conferência anual de tecnologia atraiu milhares de participantes de todo o mundo."
+    },
+    {
+        id: "4",
+        title: "Prêmios de Inovação",
+        date: "22/08/2024",
+        category: "Saúde",
+        poster: ExmapleNews,
+        description: "Nossa empresa recebeu vários prêmios por suas inovações no setor de tecnologia."
+    },
+    {
+        id: "5",
+        title: "Expansão Internacional",
+        date: "25/08/2024",
+        category: "Cultura",
+        poster: ExmapleNews,
+        description: "Estamos expandindo nossas operações para novos mercados na Ásia e Europa."
+    },
+    {
+        id: "6",
+        title: "Nova Filial Aberta",
+        date: "28/08/2024",
+        category: "Saúde",
+        poster: ExmapleNews,
+        description: "Uma nova filial foi aberta em São Paulo, aumentando nossa presença no Brasil."
+    },
+    {
+        id: "7",
+        title: "Campanha de Marketing",
+        date: "30/08/2024",
+        poster: ExmapleNews,
+        category: "Laser",
+        description: "Lançamos uma campanha de marketing inovadora que está atraindo muita atenção."
+    },
+    {
+        id: "8",
+        title: "Relatório Anual de Sustentabilidade",
+        date: "02/09/2024",
+        poster: ExmapleNews,
+        category: "Esporte",
+        description: "O relatório deste ano destaca nossos esforços contínuos para a sustentabilidade."
+    },
+];
