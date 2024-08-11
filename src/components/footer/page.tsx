640099c8a2f6f7bf0b9a282add3aabd76d13f4eb
Angeluz20 <estevao.silva015@gmail.com>
2024-08-11 19:36:32 -0400

    [fix] correct postal code in footer section

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="border-t-2 border-gray-200 w-full h-20 flex items-center justify-center bg-white">
-      <p className="text-sm text-gray-400">&copy; 2024 BOA VISTA DO RAMOS/AM - CEP 195-000</p>
+      <p className="text-sm text-gray-400">&copy; 2024 BOA VISTA DO RAMOS/AM - CEP 69.195-000</p>
     </footer>
   );
 }
