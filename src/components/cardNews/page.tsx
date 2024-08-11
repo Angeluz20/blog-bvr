261f7cdebfc9ffd8e2e289cb5d4d708b6ab0b3f6
Angeluz20 <estevao.silva015@gmail.com>
2024-08-11 19:37:12 -0400

    [feat] add animations to CardNews component

--- src/components/cardNews/page.tsx
+++ src/components/cardNews/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Image from "next/image";
-import { NewsProps } from "@/utils/types/news";
+import { NewsProps } from "../../@types/news";
 import Button from "../buttonGeneric/page";
-
+import { motion } from "framer-motion"
 export default function CardNews({
   id,
   title,
@@ -10,19 +10,30 @@ export default function CardNews({
   poster,
   description,
   isDescription,
-  idDetails = "" }: NewsProps) {
+}: NewsProps) {
   return (
-    <div
+    <motion.div
+      initial="hidden" animate="visible" variants={{
+        hidden: {
+          scale: .8,
+          opacity: 0,
+        },
+        visible: {
+          scale: 1,
+          opacity: 1,
+          transition: { delay: .02 },
+        },
+      }}
       key={id}
-      className={`flex shadow-xl items-center ${isDescription ? 'h-auto' : 'h-96'} w-96 justify-between flex-col p-2 rounded-md transition-transform transform hover:scale-105`}
+      className="flex shadow-xl items-center h-auto w-96 justify-between bg-white flex-col p-2 rounded-md transition-transform transform hover:scale-105"
     >
       <Image src={poster} className="flex w-full rounded-md" alt="poster" />
       <div className="flex flex-col justify-between w-full text-gray-500 text-sm">
-        <div className="flex items-center justify-between">
-          <p className="text-xl text-black"><strong>{title}</strong></p>
+        <div className="flex items-start mt-2 justify-between flex-col">
+          <p className="text-xl p-2 text-black"><strong>{title}</strong></p>
           <p className="p-2">Data: {date}</p>
         </div>
-        <div className="w-full flex flex-col">
+        <div className="w-full flex flex-col overflow-hidden text-ellipsis whitespace-nowrap">
           {isDescription && (
             <p className="text-justify box-border p-2">
               {description}
@@ -30,10 +41,10 @@ export default function CardNews({
           )}
           <Button
             title="Acessar conteúdo"
-            linkRoute={`/News/${idDetails}`}
+            linkRoute={`/News/${id}`}
           />
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
